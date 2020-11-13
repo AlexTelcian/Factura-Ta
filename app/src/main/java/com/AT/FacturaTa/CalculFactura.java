6002dc839ab3bbd55797899f873a84dd657a735c
AlexTelcian <alexandru.telcian99@e-uvt.ro>
2020-11-13 21:12:10 +0200

    Factura Ta update

--- app/src/main/java/com/AT/FacturaTa/CalculFactura.java
+++ app/src/main/java/com/AT/FacturaTa/CalculFactura.java
@@ -1,31 +1,20 @@
-package com.example.calculfactura;
+package com.AT.FacturaTa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import android.widget.Toast;
 
 public class CalculFactura extends AppCompatActivity {
-
-    private AdView adView;
-    private InterstitialAd mInterstitialAd;
 
     private TextView valoare1, suma1,valoareCantitate;
     private EditText pretUnitTXT, cantitateTXT,tvaTXT,markupTXT;
     private Button calculeaza,reset;
+    private float floatVal;
     private double pretUnitar,suma,valoarePret,tvaDouble,markupDouble;;
     private int sumaFactura,cantitateINT,valoareCantitateINT,sumaValoare,index;
     private String pret,cantitate,nouaValoareView,valCantView,SumaTotView,tva,markup;
@@ -34,32 +23,6 @@ public class CalculFactura extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calcul_factura);
-
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
-        });
-
-        adView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        adView.loadAd(adRequest);
-        adView = new AdView(this);
-        adView.setAdSize(AdSize.BANNER);
-        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
-
-        mInterstitialAd = new InterstitialAd(this);
-        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
-        mInterstitialAd.loadAd(new AdRequest.Builder().build());
-
-        mInterstitialAd.setAdListener(new AdListener() {
-            @Override
-            public void onAdClosed() {
-                // Load the next interstitial.
-                mInterstitialAd.loadAd(new AdRequest.Builder().build());
-            }
-
-        });
 
 
         pretUnitTXT = findViewById(R.id.PretUnitar);
@@ -77,28 +40,29 @@ public class CalculFactura extends AppCompatActivity {
         calculeaza.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 pret = pretUnitTXT.getText().toString();
+                pret = pret.replace(",",".");
                 cantitate = cantitateTXT.getText().toString();
                 tva = tvaTXT.getText().toString();
                 markup = markupTXT.getText().toString();
 
-                if(markup.length()==0)
+                if (markup.length() == 0)
                     markupTXT.setHint("necompletat");
                 else
                     index++;
-                if(tva.length()==0)
+                if (tva.length() == 0)
                     tvaTXT.setHint("necompletat");
                 else
                     index++;
-                if(pretUnitTXT.length()==0)
+                if (pretUnitTXT.length() == 0)
                     pretUnitTXT.setHint("necompletat");
                 else
                     index++;
-                if(cantitateTXT.length()==0)
+                if (cantitateTXT.length() == 0)
                     cantitateTXT.setHint("necompletat");
                 else
                     index++;
 
-                if(index >= 4) {
+                if (index >= 4) {
                     pretUnitar = Double.valueOf(pret);
                     cantitateINT = Integer.valueOf(cantitate);
                     tvaDouble = Double.valueOf(tva);
@@ -126,12 +90,6 @@ public class CalculFactura extends AppCompatActivity {
         });
         reset.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                } else {
-                    Log.d("TAG", "The interstitial wasn't loaded yet.");
-                }
 
                 pretUnitar = 0;
                 sumaValoare = (int)Math.round(pretUnitar);
